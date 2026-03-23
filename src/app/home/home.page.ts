8803d88bf0d8982f949fa7a512ff3ad80b24c299
unknown <roisinpinches@gmail.com>
2026-03-23 10:45:09 +0000

    'services'

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,27 @@
 import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import { IonHeader, IonToolbar, IonTitle, IonContent, IonCard, IonCardHeader, IonCardTitle, IonCardSubtitle } from '@ionic/angular/standalone';
+import { MovieService } from '../services/movie-service';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent],
+  imports: [IonHeader, IonToolbar, IonTitle, IonContent, IonCard, IonCardHeader, IonCardTitle, IonCardSubtitle],
 })
 export class HomePage {
-  constructor() {}
+  //create movie array
+  movies: any[] = [];
+
+  //gets the movie data
+  constructor(private service: MovieService) { }
+  ionViewWillEnter() {
+    this.service.GetShrekMovieData().subscribe(
+      (data) => {
+        this.movies = data.Search;
+        console.log(this.movies);
+      }
+    );
+
+  }
 }
+
